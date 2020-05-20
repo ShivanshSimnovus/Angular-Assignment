f9a0a8bce7e1b0ec4c2802301677e864bf812011
ShivanshSimnovus <shivansh.pandey@simnovus.com>
2020-05-20 23:38:09 +0530

    Login form validation has been added in the login page as mentioned in the assignment. Password is checked for length and space. Email is checked for proper format.

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  onSubmit(email: string, password: string) {
+    var message: string = "";
+    var atpos = email.indexOf("@");
+    var dotpos = email.lastIndexOf(".");
+    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length) {
+      message = "e-mail is not valid.";
+    }
+    if (password.length < 10 || password.length > 16 || password.indexOf(" ") != -1) {
+      message = message+" Password length should be between 10 to 16 characters with no space.";
+    }
+    if(message.length!=0)
+    {
+      alert(message);
+    }
+    else {
+      alert("Everything Looks good");
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
